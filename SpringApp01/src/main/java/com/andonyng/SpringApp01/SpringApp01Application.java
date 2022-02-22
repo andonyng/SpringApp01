2d6403e32e7cf9f2b0c9f958a631053ba0d287bb
Andony Ng <andony.ng.11@gmail.com>
2022-02-22 12:44:54 +0100

    Creado Proyecto Spring Java 11

---
+++ SpringApp01/src/main/java/com/andonyng/SpringApp01/SpringApp01Application.java
@@ -0,0 +1,13 @@
+package com.andonyng.SpringApp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp01Application.class, args);
+	}
+
+}
